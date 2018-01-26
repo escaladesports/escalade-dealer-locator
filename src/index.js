4926fb9121cebd765438591beaa7b825cc6f2ee3
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-26 10:09:32 -0600

    Default map options

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React from 'react'
+import Map from 'google-map-react'
+
+import defaultMapOptions from './default-map-options'
 
 export default class extends React.Component {
 	constructor(props){
 		super(props)
+		this.createMapOptions = this.createMapOptions.bind(this)
+	}
+	createMapOptions(){
+		return {
+			...defaultMapOptions,
+			...this.props.options,
+		}
 	}
 	render(){
 		return (
-			<div>
-				This is a test
-				<style jsx>{`
-					div{
-						color: red;
+			<div className='escaDealers'>
+				<Map
+					options={this.createMapOptions}
+					defaultCenter={this.props.center || {
+						lat: 39,
+						lng: -95,
+					}}
+					defaultZoom={this.props.zoom || 3}
+				/>
+				<style jsx='true'>{`
+					.escaDealers{
+						height: 600px;
+						width: 100%;
 					}
 				`}</style>
 			</div>
